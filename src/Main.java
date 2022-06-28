4a8be13bf12dad3ec5ea2031b4e196286a5b6228
Baisalya <baishalya1999@gmail.com>
2022-06-28 13:33:58 +0530

    added route to ticket page search panel

--- src/Main.java
+++ src/Main.java
@@ -81,6 +81,11 @@ public class Main extends javax.swing.JFrame {
         jMenu2.setText("Tickets");
 
         jMenuItem3.setText("Book Ticket");
+        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem3ActionPerformed(evt);
+            }
+        });
         jMenu2.add(jMenuItem3);
 
         jMenuBar1.add(jMenu2);
@@ -140,6 +145,13 @@ public class Main extends javax.swing.JFrame {
         jDesktopPane1.add(scus);
         scus.setVisible(true);
     }//GEN-LAST:event_jMenuItem2ActionPerformed
+
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+        // TODO add your handling code here:
+         TicketBooking scus=new TicketBooking();
+        jDesktopPane1.add(scus);
+        scus.setVisible(true);
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
 
     /**
      * @param args the command line arguments
